97367f9e6aafdccccc2c0b5e76d446de76ecf5f2
James Warren <jwarren116@gmail.com>
2015-02-10 13:33:01 -0800

    Outline of Echo Client that comnects to Echo Server socket and sends message, once message returned connection is closed

---
+++ echo_client.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+
+def client(msg):
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    client_socket.connect(('127.0.0.1', 50000))
+
+    # sends command line message to server, closes socket to writing
+    client_socket.sendall(msg)
+    client_socket.shutdown(socket.SHUT_WR)
+
+    # receives and prints incoming echo response, then closes connection
+    response = client_socket.recv(16)
+    print response
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    client(sys.argv[1])
