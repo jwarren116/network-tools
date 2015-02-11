1ddf9c7c2101b7fabca24f937e3d82ff5ab76f4c
James Warren <jwarren116@gmail.com>
2015-02-10 17:37:14 -0800

    Fix bug that was closing connection before completion

--- echo_client.py
+++ echo_client.py
@@ -8,18 +8,22 @@ def client(msg):
         socket.SOCK_STREAM,
         socket.IPPROTO_IP)
     client_socket.connect(('127.0.0.1', 50000))
-    buffer = 16
 
     # sends command line message to server, closes socket to writing
     client_socket.sendall(msg)
     client_socket.shutdown(socket.SHUT_WR)
 
-    response = client_socket.recv(16)
-    while len(response) >= buffer:
-        # receives and prints incoming echo response, then closes connection
-        response += client_socket.recv(16)
-    print response
-    client_socket.close()
+    buffsize = 32
+    response_msg = ''
+    done = False
+    while not done:
+        msg_part = client_socket.recv(buffsize)
+        if len(msg_part) < buffsize:
+            done = True
+            client_socket.close()
+        response_msg += msg_part
+
+    print response_msg
 
 
 if __name__ == '__main__':
--- echo_server.py
+++ echo_server.py
@@ -2,18 +2,26 @@ import socket
 
 
 try:
-    while True:
-        server_socket = socket.socket(
-            socket.AF_INET,
-            socket.SOCK_STREAM,
-            socket.IPPROTO_TCP)
-        server_socket.bind(('127.0.0.1', 50000))
-        server_socket.listen(1)
-        connection, client_address = server_socket.accept()
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_TCP)
+    server_socket.bind(('127.0.0.1', 50000))
+    server_socket.listen(1)
 
-        # receive message from client, and immediately return
-        echo_msg = connection.recv(16)
-        connection.sendall(echo_msg)
+    while True:
+        connection, client_address = server_socket.accept()
+        buffsize = 32
+        response_msg = ''
+        done = False
+        while not done:
+            echo_msg = connection.recv(buffsize)
+            if len(echo_msg) < buffsize:
+                done = True
+                # connection.close()
+            response_msg += echo_msg
+            print response_msg
+        connection.sendall(response_msg)
 
         # shutdown socket to writing after sending echo message
         connection.shutdown(socket.SHUT_WR)
