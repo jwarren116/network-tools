2307942c6e29ae24d6d4fdc42a646e4f6843fca8
James Warren <jwarren116@gmail.com>
2015-02-10 16:51:44 -0800

    Update client to receive packets larger than buffer

--- echo_client.py
+++ echo_client.py
@@ -8,13 +8,16 @@ def client(msg):
         socket.SOCK_STREAM,
         socket.IPPROTO_IP)
     client_socket.connect(('127.0.0.1', 50000))
+    buffer = 16
 
     # sends command line message to server, closes socket to writing
     client_socket.sendall(msg)
     client_socket.shutdown(socket.SHUT_WR)
 
-    # receives and prints incoming echo response, then closes connection
     response = client_socket.recv(16)
+    while len(response) >= buffer:
+        # receives and prints incoming echo response, then closes connection
+        response += client_socket.recv(16)
     print response
     client_socket.close()
 
